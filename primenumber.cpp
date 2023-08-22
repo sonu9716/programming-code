9843ba1bb1b42d1596f0b46ad2849e062d0b1a65
sonu9716 <sonusingh2972001@gmail.com>
2023-08-22 10:43:51 +0530

    prime number

---
+++ primenumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, div, flag = 0;
+    div = 2;
+    cin >> n;
+    int m = n / 2;
+    for (int i = 2; i <= m; i++)
+    {
+        if (n % div == 0)
+        {
+            cout << "not a prime number ";
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+    {
+        cout << "this is a prime number ";
+        return 1;
+    }
+}
