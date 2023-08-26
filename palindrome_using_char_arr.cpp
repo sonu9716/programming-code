70d4f9243d836d5a7370c21bb7bb86888dd4e882
sonu9716 <sonusingh2972001@gmail.com>
2023-08-26 13:51:58 +0530

    palindrome using char array

---
+++ palindrome_using_char_arr.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    char arr[n + 1];
+    cin >> arr;
+    bool flag = 1;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] != arr[n - 1 - i])
+        {
+            flag = 0;
+            break;
+        }
+        if (flag == 1)
+        {
+            cout << "It is a palindrome " << endl;
+        }
+        else
+        {
+            cout << "it is not a palindrome " << endl;
+        }
+        return 0;
+    }
+}
